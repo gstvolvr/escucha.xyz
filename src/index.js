a1007f2fb5ab93b4d71854abf9b88307c7fb20ba
Gustavo A. Oliver <g.antonio.oliver@gmail.com>
2019-07-14 19:39:29 -0400

    use react's context api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Recommender from './components/Recommender'
+import Firebase from './components/Firebase'
+import FirebaseContext from './components/FirebaseContext'
 
-var root = document.getElementById('root')
 ReactDOM.render(
-  <Recommender />,
-  root
-);
+  <FirebaseContext.Provider value={new Firebase()}>
+    <FirebaseContext.Consumer>{firebase =>
+      <Recommender firebase={firebase} />}
+    </FirebaseContext.Consumer>
+  </FirebaseContext.Provider>,
+  document.getElementById('root')
+)
