db630b8d493abc5df9dc10b2129451e0032b253c
Gustavo A. Oliver <g.antonio.oliver@gmail.com>
2019-07-28 22:09:07 -0400

    default image location had to change

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -14,7 +14,7 @@ export default class Artist extends React.Component {
     return (
       <a target="_blank" rel="noopener noreferrer" href={this.props.url}>
         <Figure>
-          <Figure.Image bsPrefix=".img" height={imgSize} width={imgSize} fluid rounded src={this.props.src}/>
+          <Figure.Image bsPrefix=".img" height={imgSize} width={imgSize} fluid rounded src={this.props.src.replace("www.olvr.xyz", "www.gustavooliver.xyz")}/>
           <Figure.Caption>{this.props.name}</Figure.Caption>
         </Figure>
       </a>
